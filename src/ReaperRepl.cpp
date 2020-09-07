2d9abaea92b5bad33231454c3dcea458de28bee4
actondev <chris.actondev@gmail.com>
2020-09-07 17:45:05 +0200

    SDL -> SFML gui working

--- src/ReaperRepl.cpp
+++ src/ReaperRepl.cpp
@@ -10,10 +10,6 @@
 #include "s7_reaper/path.hpp"
 #include "aod/s7.hpp"
 
-#include "imgui.h"
-#include "imgui_impl_sdl.h"
-#include "imgui_impl_opengl2.h"
-#include "SDL_opengl.h"
 
 
 using std::cout;
@@ -21,138 +17,8 @@ using std::cerr;
 using std::endl;
 namespace fs = std::filesystem;
 
-
+#include "imgui.h"
 thread_local ImGuiContext* MyImGuiTLS = NULL;
-
-
-int guiLoop() {
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        printf("Error: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
-
-
-    SDL_Window* window = SDL_CreateWindow("s7imgui - Gui Repl", 100, 100, 600, 400,
-                                          // SDL_WINDOWPOS_CENTERED caused the app to not show on linux. wtf
-                                          SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-
-    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    SDL_GL_MakeCurrent(window, gl_context);
-    SDL_GL_SetSwapInterval(1);    // Enable vsync
-
-// Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-
-    ImGuiIO& io = ImGui::GetIO();
-    (void) io;
-    ImGui::StyleColorsDark();
-
-    bool sdlInit = ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-    ImGui_ImplOpenGL2_Init();
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-    ImGui::PushStyleColor(ImGuiCol_WindowBg, IM_COL32(30, 30, 30, 255));
-
-//     s7_pointer setup_fn = s7_name_to_value(sc, "setup");
-//     if (setup_fn != s7_undefined(sc)) {
-//         s7_call(sc, setup_fn, s7_nil(sc));
-//     }
-
-    //While application is running
-//     bool have_drawn = false;
-    unsigned int redraws_after_no_events = 0;
-    bool running = true;
-    // TODO remove this
-    bool g_force_redraw = false;
-    bool show_demo_window = true;
-    while (running) {
-
-        bool have_events = false;
-//         std::unique_lock<std::mutex> lock_loop(g_gui_loop_mutex);
-
-        SDL_Event e;
-
-        //Handle events on queue
-        while (SDL_PollEvent(&e) != 0) {
-//            printf("SDL event\n");
-            have_events = true;
-            redraws_after_no_events = 0;
-            //User requests quit
-            switch (e.type) {
-            case SDL_QUIT:
-                printf("SDL_QUIT event\n");
-                running = false;
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-            case SDL_MOUSEBUTTONUP:
-//                     fprintf(stderr, "Mouse down/up at (%d,%d)\n", e.motion.x,
-//                             e.motion.y);
-                break;
-            }
-            ImGui_ImplSDL2_ProcessEvent(&e);
-        }
-        if (!running) {
-            break;
-        }
-
-        if (redraws_after_no_events > 2 && !g_force_redraw) {
-            // no need to redraw!
-//             lock_loop.unlock();
-            std::this_thread::sleep_for(std::chrono::milliseconds(10));
-            continue;
-        }
-        g_force_redraw = false;
-
-        if (!have_events) {
-            redraws_after_no_events++;
-        }
-
-        ImGui_ImplOpenGL2_NewFrame();
-        ImGui_ImplSDL2_NewFrame(window);
-        ImGui::NewFrame();
-
-//         s7_eval_c_string(sc, "(draw)");
-
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
-
-
-        ImGui::Render();
-
-        glViewport(0, 0, (int) io.DisplaySize.x, (int) io.DisplaySize.y);
-        // glClearColor is freezing if the window has been closed
-        glClearColor((GLclampf) clear_color.x, (GLclampf) clear_color.y,
-                     (GLclampf) clear_color.z, (GLclampf) clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
-        //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
-        ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(window);
-
-//         lock_loop.unlock();
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    }
-    printf("guiLoop: quit gui event loop, cleaning up \n");
-
-    ImGui_ImplOpenGL2_Shutdown();
-    ImGui_ImplSDL2_Shutdown();
-    ImGui::DestroyContext();
-
-    SDL_GL_DeleteContext(gl_context);
-
-    SDL_DestroyWindow(window);
-
-    SDL_Quit();
-
-    printf("guiLoop: ----- gui loop quit ------\n");
-    return 0;
-}
 
 ReaperRepl::ReaperRepl(reaper_plugin_info_t* pRec)
     : ReaperExtBase(pRec)
@@ -190,15 +56,9 @@ ReaperRepl::ReaperRepl(reaper_plugin_info_t* pRec)
         InsertTrackAtIndex(GetNumTracks(), false);
     };
 
-    auto action_imgui = []() {
-        new std::thread(guiLoop);
-    };
-
     //Register an action. args: name: lambda, add menu item,
     RegisterAction("IPlugReaperExtension: Action 1 - MsgBox", action1, true);
     RegisterAction("IPlugReaperExtension: Action 2 - AddTrack", action2, true);
-    RegisterAction("imgui demo", action_imgui, true);
-    // RegisterAction("IPlugReaperExtension: Action 3 - Show/Hide UI", [&]() { ShowHideMainWindow(); mGUIToggle = !mGUIToggle; }, true, &mGUIToggle);
 
     cb = [&](const char *data) -> std::string {
 //         printf("main: got data %s\n", data);
